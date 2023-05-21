cd2b2893a03d7679fcaa07cceccfd5f14deb871e
Wanderson A. Timóteo <wanderson_timoteo@hotmail.com>
2023-05-21 19:46:05 -0400

    Separado as rotas em memories.ts e PrismaClient em prisma.ts

---
+++ server/src/routes/memories.ts
@@ -0,0 +1,13 @@
+import { FastifyInstance } from 'fastify'
+
+export async function memoriesRoutes(app: FastifyInstance) {
+  app.get('/memories', async () => {})
+
+  app.get('/memories/:id', async () => {})
+
+  app.post('/memories', async () => {})
+
+  app.put('/memories/:id', async () => {})
+
+  app.delete('/memories/:id', async () => {})
+}
