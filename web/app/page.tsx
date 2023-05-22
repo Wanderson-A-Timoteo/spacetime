4896d89799ffeef6aa3ab8f224e6080922c219e5
Wanderson A. Timóteo <wanderson_timoteo@hotmail.com>
2023-05-22 17:54:34 -0400

    Adicionado componente SignIn

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,9 +1,6 @@
-import { User } from 'lucide-react'
-import Image from 'next/image'
-import nlwLogo from '../src/assets/nlw-spacetime-logo.svg'
-import Link from 'next/link'
 import Copyright from '@/src/components/Copyright'
 import Hero from '@/src/components/Hero'
+import SignIn from '@/src/components/SignIn'
 
 export default function Home() {
   return (
@@ -11,19 +8,8 @@ export default function Home() {
       <div className="border-white/10 relative flex flex-col items-start justify-between overflow-hidden border-r bg-[url(./../src/assets/bg-stars.svg)] bg-cover px-28 py-16">
         <div className="blur-full absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50" />
         <div className="bg-stripes absolute bottom-0 right-2 top-0 w-2" />
-        <a
-          href={`https://github.com/login/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID}`}
-          className="flex items-center gap-3 text-left transition-colors hover:text-gray-50"
-        >
-          <div className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-400">
-            <User className="h-5 w-5 text-gray-500" />
-          </div>
-          <p className="max-w-[140px] text-sm leading-snug">
-            <span className="underline"> Crie sua conta </span> e salve suas
-            memórias!
-          </p>
-        </a>
 
+        <SignIn />
         <Hero />
         <Copyright />
       </div>
