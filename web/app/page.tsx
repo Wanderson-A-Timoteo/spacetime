ea2992463bd13f89da05cb7968a0d8b2e6bbb8ff
Wanderson A. Timóteo <wanderson_timoteo@hotmail.com>
2023-05-22 19:24:27 -0400

    Adicionado jwt-decode, componente Profile e criado auth.ts para usar os cookies

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,16 +1,19 @@
+import { cookies } from 'next/headers'
 import Copyright from '@/src/components/Copyright'
 import EmptyMemories from '@/src/components/EmpytMemories'
 import Hero from '@/src/components/Hero'
 import SignIn from '@/src/components/SignIn'
+import Profile from '@/src/components/Profile'
 
 export default function Home() {
+  const isAuthenticated = cookies().has('token')
   return (
     <main className="grid min-h-screen grid-cols-2">
       <div className="border-white/10 relative flex flex-col items-start justify-between overflow-hidden border-r bg-[url(./../src/assets/bg-stars.svg)] bg-cover px-28 py-16">
         <div className="blur-full absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50" />
         <div className="bg-stripes absolute bottom-0 right-2 top-0 w-2" />
 
-        <SignIn />
+        {isAuthenticated ? <Profile /> : <SignIn />}
         <Hero />
         <Copyright />
       </div>
